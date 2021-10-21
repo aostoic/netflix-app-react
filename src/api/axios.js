09aa8b3371ee8cf015c99c4d60a3e72edc389305
andro ostoic <androstoic@gmail.com>
2021-10-21 15:19:37 -0300

    feat : primer commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://localhost:3030/",
-  baseURL: "https://netflix-api-example.herokuapp.com/films",
+  baseURL: "https://netflix-api-example.herokuapp.com/",
 });
 
 export default instance;
